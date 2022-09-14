596462c5d45c478c52e74647d7b4c8fe48f52b87
jpachino <106620083+jpachino@users.noreply.github.com>
2022-09-14 21:22:55 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-function openMenu(evt, menuName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("menu");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
-    }
-    document.getElementById(menuName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+    
+  let slides = document.getElementsByClassName("slider__slide");
+  let navlinks = document.getElementsByClassName("slider__navlink");
+  let currentSlide = 0;
+  
+  document.getElementById("nav-button--next").addEventListener("click", () => {
+      changeSlide(currentSlide + 1)
+  });
+  document.getElementById("nav-button--prev").addEventListener("click", () => {
+      changeSlide(currentSlide - 1)
+  });
+  
+  function changeSlide(moveTo) {
+      if (moveTo >= slides.length) {moveTo = 0;}
+      if (moveTo < 0) {moveTo = slides.length - 1;}
+     
+      slides[currentSlide].classList.toggle("active");
+      navlinks[currentSlide].classList.toggle("active");
+      slides[moveTo].classList.toggle("active");
+      navlinks[moveTo].classList.toggle("active");
+     
+      currentSlide = moveTo;
   }
-  document.getElementById("myLink").click();
+  
+  document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+      bullet.addEventListener('click', () => {
+          if (currentSlide !== bulletIndex) {
+              changeSlide(bulletIndex);
+          }
+      })
+  })
+    
